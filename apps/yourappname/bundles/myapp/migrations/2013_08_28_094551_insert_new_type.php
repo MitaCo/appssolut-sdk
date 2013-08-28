d4e7b2f478d1e1139dddf524ecc31eb483d60b13
tijl-verbert <tijl.verbert@digitalmagics.com>
2013-08-28 12:00:13 +0200

    comments

--- apps/yourappname/bundles/myapp/migrations/2013_08_28_094551_insert_new_type.php
+++ apps/yourappname/bundles/myapp/migrations/2013_08_28_094551_insert_new_type.php
@@ -7,6 +7,7 @@ class Myapp_Insert_New_Type {
         // "type" must be same name as the type partial
         DB::query("insert into types (id, type, name, created_at, updated_at) values (22, 'mytype-paragraph', 'My Type Paragraph', '2013-06-10 12:00:00', '2013-06-10 12:00:00');");
 
+        // Set new id sequence
         DB::query("SELECT setval('types_id_seq', 22);");
 	}
 
@@ -14,6 +15,7 @@ class Myapp_Insert_New_Type {
 	{
         DB::query("delete from types where id = 22");
 
+        // Put back old id sequence
         DB::query("SELECT setval('types_id_seq', 21);");
 	}
 
